ee0c848a6c566110c38304dbd9d16e79df651ef0
Kingstalux <kingsleyngu25@gmail.com>
2022-05-14 11:22:21 +0100

    Add route and controller

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,7 @@
+class V1::GreetingsController < ApplicationController
+    def index
+      @random = Greeting.find(Greeting.pluck(:id).sample)
+      render json: @random, status: :ok
+    end
+  end
+  
